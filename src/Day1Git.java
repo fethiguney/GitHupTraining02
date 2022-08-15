bfc71c71fa261b92f7456b72f26ce9f7e616d56a
Fethi <fethiguneyy@gmail.com>
2022-08-15 22:23:21 +0300

    V1

---
+++ src/Day1Git.java
@@ -0,0 +1,13 @@
+public class Day1Git {
+
+    public static void main(String[] args) {
+
+        System.out.println("Git 1. Deneme");
+        System.out.println("Git 2. Deneme");
+
+
+
+
+
+    }
+}
