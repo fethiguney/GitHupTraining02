7c35f0c42006c1e50a8edf84e1f03bba2ed368a9
Fethi <fethiguneyy@gmail.com>
2022-08-15 22:27:33 +0300

    V2

--- src/Day1Git2.java
+++ src/Day1Git2.java
@@ -3,7 +3,30 @@ public class Day1Git2 {
     public static void main(String[] args) {
 
 
-        System.out.println("Git 2.Class");
+        /*
+ 1-) git init --> Local repo olusturmak icin yani  .git ile klasörümüzün
+ içindeki dosyaları
+ ilişkilendirmek için kullanılır
+ 2-) git add . --> Working space'den (yani yerel) dosyalarımı staging area'ya
+ (yani commitlemek için beklenen yer) gönderir
+ 3-) git commit -m "mesaj" --> Staging area'dan commit stora
+ dosyalarımı göndermek için kullanılır
+ (commit yani versiyon yani sürüm oluşturmuş olurum)
+ 4-) git push --> Uzak repo'ya(yani remote - GitHub)
+  göndermek için kullandıgımız kod
+ yalnız git push komutunu diirekt kullanmak istersek 1 kez
+         git remote add origin adress
+         git push -u origin master
+NOT: Yukarıdaki iki komutu tek seferde kullandıktan sonra ikinci commit'lerim için sadece git push
+kullanırız
+
+Not: git log --oneline --> commitlerimdeki yani
+versiyonlarimdaki durumu gosterir
+
+git commit -a -m --> bir defa git add . yaptiktan sonra
+bu komut ile versiyon degisikligi yaptiktan sonra
+tekrar git add yapmadan commitliyebiliyoruz
+  */
 
 
 
